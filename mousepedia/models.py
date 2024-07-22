a36600f4ceaec64d947733e372f36a1b67d134cb
Varinia Butcher <varinia_b@hotmail.com>
2024-07-22 19:32:51 +0000

    define class models

---
+++ mousepedia/models.py
@@ -0,0 +1,52 @@
+from mousepedia import db
+
+
+class Park(db.Model):
+    #schema for the Parks model
+    __tablename__ = 'parks'
+    id = db.Column(db.integer, primary_key=True)
+    park_name = db.Column(db.String(50), unique=True, nullable=False)
+    park_address = db.Column(db.String(50), unique=True, nullable=False)
+    park_description = db.Column(db.Text, nullable=False)
+    date_opened = db.Column(db.Date, nullable=False)
+    opening_times = db.Column(db.String(20), nullable=False)
+    num_restaurants = db.Column(db.Integer, nullable=False)
+    restaurant_ids = db.Column(db.Integer, db.ForeignKey("restaurant.id", ondelete="CASCADE"), nullable=False)
+    num_rides = db.Column(db.Integer, nullable=False)
+    ride_ids = db.Column(db.Integer, db.ForeignKey("ride.id", ondelete="CASCADE"), nullable=False)
+    entertainment = db.Column(db.Text, nullable=False)
+    special_features = db.Column(db.Text, nullable=False)
+    transport_between_parks = db.Column(db.Text, nullable=False)
+    restaurants = db.relationship("Restaurant", backref="park", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.park_name
+
+
+
+class Restaurant(db.Model):
+    #schema for the Restaurants model
+    __tablename__ = 'restaurants'
+    id = db.Column(db.integer, primary_key=True)
+    restaurant_name = db.Column(db.String(50), unique=True, nullable=False)
+    park_id = db.Column(db.Integer, db.ForeignKey("park.id"), nullable=False)
+    restaurant_location = db.Column(db.Text, nullable=False)
+    dine_or_quick_service = db.Column(db.Boolean, default=False, nullable=False)
+
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self
+
+
+
+class Ride(db.Model):
+    #schema for the Rides model
+    __tablename__ = 'rides'
+    id = db.Column(db.integer, primary_key=True)
+    ride_name = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self
