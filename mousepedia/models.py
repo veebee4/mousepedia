dbf4790a44edffeb2c4edf9df0ffaf8b7f21e78f
Varinia Butcher <varinia_b@hotmail.com>
2024-07-24 19:12:06 +0000

    Fix: issue with being unable to populate tables to database

--- mousepedia/models.py
+++ mousepedia/models.py
@@ -1,10 +1,43 @@
 from mousepedia import db
 
 
+
+class Restaurant(db.Model):
+    #schema for the Restaurants model
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_name = db.Column(db.String(50), unique=True, nullable=False)
+    park_id = db.Column(db.Integer, db.ForeignKey("park.id"), nullable=False)
+    restaurant_location = db.Column(db.Text, nullable=False)
+    dine_or_quick_service = db.Column(db.Boolean, default=False, nullable=False)
+    food_type = db.Column(db.String(30), nullable=False)
+    restaurant_description = db.Column(db.Text, nullable=False)
+    parks = db.relationship("Park", backref="restaurant", cascade="all, delete", lazy=True)
+
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.restaurant_name
+
+
+
+class Ride(db.Model):
+    #schema for the Rides model
+    id = db.Column(db.Integer, primary_key=True)
+    ride_name = db.Column(db.String(50), unique=True, nullable=False)
+    park_id = db.Column(db.Integer, db.ForeignKey("park.id", ondelete="CASCADE"), nullable=False)
+    ride_location = db.Column(db.String(50), nullable=False)
+    ride_description = db.Column(db.Text, nullable=False)
+    parks = db.relationship("Park", backref="ride", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.ride_name
+
+
+
 class Park(db.Model):
     #schema for the Parks model
-    __tablename__ = 'parks'
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     park_name = db.Column(db.String(50), unique=True, nullable=False)
     park_address = db.Column(db.String(50), unique=True, nullable=False)
     park_description = db.Column(db.Text, nullable=False)
@@ -23,38 +56,3 @@ class Park(db.Model):
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.park_name
-
-
-
-class Restaurant(db.Model):
-    #schema for the Restaurants model
-    __tablename__ = 'restaurants'
-    id = db.Column(db.integer, primary_key=True)
-    restaurant_name = db.Column(db.String(50), unique=True, nullable=False)
-    park_id = db.Column(db.Integer, db.ForeignKey("park.id"), nullable=False)
-    restaurant_location = db.Column(db.Text, nullable=False)
-    dine_or_quick_service = db.Column(db.Boolean, default=False, nullable=False)
-    food_type = db.Column(db.String(30), nullable=False)
-    restaurant_description = db.Column(db.Text, nullable=False)
-    parks = db.relationship("Park", backref="restaurant", cascade="all, delete", lazy=True)
-
-
-    def __repr__(self):
-        # __repr__ to represent itself in the form of a string
-        return self.restaurant_name
-
-
-
-class Ride(db.Model):
-    #schema for the Rides model
-    __tablename__ = 'rides'
-    id = db.Column(db.integer, primary_key=True)
-    ride_name = db.Column(db.String(50), unique=True, nullable=False)
-    park_id = db.Column(db.Integer, db.ForeignKey("park.id", ondelete="CASCADE"), nullable=False)
-    ride_location = db.Column(db.String(50), nullable=False)
-    ride_description = db.Column(db.Text, nullable=False)
-    parks = db.relationship("Park", backref="ride", cascade="all, delete", lazy=True)
-
-    def __repr__(self):
-        # __repr__ to represent itself in the form of a string
-        return self.ride_name
--- mousepedia/routes.py
+++ mousepedia/routes.py
@@ -1,6 +1,7 @@
 # import modules and functions
-from flask import render_template
+from flask import render_template, Flask
 from mousepedia import app, db
+from mousepedia.models import Park, Restaurant, Ride
 
 @app.route("/")
 def home():
