0baf25cb8a7294bd3adcb3e28df84ee47a7da55b
Varinia Butcher <varinia_b@hotmail.com>
2024-08-01 20:00:12 +0000

    Add form for add_park.html

--- mousepedia/static/js/script.js
+++ mousepedia/static/js/script.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // initialises the side nav bar
-    let sidenavbar = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenavbar);
+  // initialises the side nav bar
+  let sidenavbar = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenavbar);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // initialises the date picker for date_opened on the add_park form 
+  let dateOpened = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(dateOpened);
   });
+
+  $('.materialize-textarea').val('New Text');
+  M.textareaAutoResize($('.materialize-textarea'));
