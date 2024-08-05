7d8c60dcc7f4dd10b1bd7ececcd86e2e411977ca
Varinia Butcher <varinia_b@hotmail.com>
2024-08-05 19:05:31 +0000

    Add more styling to add_park.html form

--- mousepedia/routes.py
+++ mousepedia/routes.py
@@ -1,5 +1,5 @@
 # import modules and functions
-from flask import render_template, Flask
+from flask import render_template, request, redirect, url_for
 from mousepedia import app, db
 from mousepedia.models import Park, Restaurant, Ride
 
@@ -13,4 +13,9 @@ def parks():
 
 @app.route("/add_park", methods=["GET", "POST"])
 def add_park():
+    if request.method == "POST":
+        park = Park(park_name=request.form.get("park_name"))
+        db.session.add()
+        db.session.commit()
+        return redirect(url_for("parks"))
     return render_template("add_park.html")
